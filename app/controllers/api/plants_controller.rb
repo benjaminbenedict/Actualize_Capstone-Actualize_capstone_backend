a6bb80b2411fb2d35e9fa4658c8b2372d65a541b
Ben Benedict <Ben.Benedict@gmail.com>
2021-03-11 21:13:21 -0600

    plants controller create, plants views show, index, partial

--- app/controllers/api/plants_controller.rb
+++ app/controllers/api/plants_controller.rb
@@ -1,11 +1,32 @@
 class Api::PlantsController < ApplicationController
   def index
-    @plants = Plants.all
-    renger "index.json.jb"
+    @plants = Plant.all
+    if params["search"]
+      @plants = @plants.where("name ILIKE ?", "%#{params[:search]}%")
+    end
+    render "index.json.jb"
   end
 
   def show
     @plant = Plant.find_by(id: params[:id])
     render "show.json.jb"
+  end
+
+  def create
+    @plant = Plant.new(
+      name: params["name"],
+      price: params["price"],
+      description: params["description"],
+      supplier_id: params["supplier_id"],
+    )
+    @plant.save
+    render "show.json.jb"
+
+    # if @plant.save
+    #   Image.create(plant_id: @plant.id, url: params[:image_url])
+    #   render "show.json.jb"
+    # else
+    #   render json: { errors: @plant.errors.full_messages }, status: 422
+    # end
   end
 end
