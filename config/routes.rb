e6a69afcb0046dcf69f828d158d0a34709005ccb
Ben Benedict <Ben.Benedict@gmail.com>
2021-03-11 19:46:24 -0600

    built initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,35 @@ Rails.application.routes.draw do
 
     get "/plants" => "plants#index"
     get "/plants/:id" => "plants#show"
+    post "/plants" => "plants#create"
+    patch "/plants/:id" => "plants#update"
+    delete "/plants/:id" => "plants#destroy"
 
     get "/user_plants" => "user_plants#index"
     get "/user_plants/:id" => "user_plants#show"
+    post "/user_plants" => "user_plants#create"
+    patch "/user_plants/:id" => "user_plants#update"
+    delete "/user_plants/:id" => "user_plants#destroy"
+
+    get "/ratings" => "ratings#index"
+    post "/ratings" => "ratings#create"
+    delete "/ratings/:id" => "ratings#destroy"
+
+    get "/locations" => "locations#index"
+    get "/locations/:id" => "locations#show"
+    post "/locations" => "locations#create"
+    delete "/locations/:id" => "locations#destroy"
+
+    get "/location_user_plants" => "location_user_plants#index"
+    get "/location_user_plants/:id" => "location_user_plants#show"
+
+    get "/waterings" => "waterings#index"
+    get "/waterings/:id" => "waterings#show"
+    post "/waterings" => "waterings#create"
+    delete "/waterings/:id" => "waterings#destroy"
+
+    get "/pictures" => "pictures#index"
+    get "/pictures/:id" => "pictures#show"
+    post "/pictures" => "pictures#create"
   end
 end
