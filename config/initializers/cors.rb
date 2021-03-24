1f62829f6642e94ff8c47cd8639e21c14738dd9a
Ben Benedict <Ben.Benedict@gmail.com>
2021-03-24 17:37:36 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "example.com"
+    origins "localhost:8080", "https://quizzical-galileo-ef4d3f.netlify.app"
 
     resource "*",
       headers: :any,
